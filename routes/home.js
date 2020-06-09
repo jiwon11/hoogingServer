80dbe85923a3a82958923dc76b6e68c60c8fe648
Jiwon <jeewonre@naver.com>
2020-06-09 16:59:13 +0900

    위도,경도 값으로 반경 안에 있는 포스트 가져오기

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { Post, Tag, User, MediaFile, Address, Comment, Like,Description } = require('../models');
+const sequelize = require('sequelize');
 const router = express.Router();
 
 
@@ -28,4 +29,33 @@ router.get('/',isLoggedIn, async(req, res, next) => {
   }
 });
 
+router.get('/address', isLoggedIn, async(req,res,next)=> {
+  try{
+    const long = req.query.long;
+    const lat = req.query.lat;
+    const radius = req.query.radius;
+    const posts = await Post.findAll({
+      include : [
+        {
+          model : Address,
+          where : sequelize.literal(`(6371*acos(cos(radians(${lat}))*cos(radians(geographLat))*cos(radians(geographLong)-radians(${long}))+sin(radians(${lat}))*sin(radians(geographLat))))<=${radius}`)
+        }
+      ],
+      where : {
+        dump : false,
+        certifiedLocation : true,
+      },
+      attributes: {
+        include: [
+          [sequelize.literal('hits*1+likes*3'), 'popular']
+        ]
+      },
+      order : [['createdAt', 'DESC']]
+    });
+    return res.status(200).json(posts);
+  } catch(error){
+    console.log(error);
+    res.status(404).json(error);
+  }
+});
 module.exports = router;
